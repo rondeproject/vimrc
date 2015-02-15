b220f137ca1c53e66955cf636faa7c21feafd81a
Ronde <none>
2015-02-15 23:36:49 +0900

    Update

--- install.sh
+++ install.sh
@@ -15,13 +15,3 @@ if [ -f $VIM_DIR/.vimrc -a ! -f $VIM_FILE ]; then
 	ln -s $VIM_DIR/.vimrc $VIM_FILE
 fi
 
-# 日本語helpのインストール
-mkdir -p $VIM_DIR/temp
-pushd $VIM_DIR/temp
-wget https://github.com/vim-jp/vimdoc-ja/archive/master.zip
-unzip master.zip
-cp -a vimdoc-ja-master/doc $VIM_DIR
-cp -a vimdoc-ja-master/syntax $VIM_DIR
-popd > /dev/null
-rm -rf $VIM_DIR/temp
-
