89da19b6976c45b1f66a90f4684f1f29c418d8d1
Ronde <none>
2015-03-05 11:22:25 +0900

    Update

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ readonly VIM_DIR=$HOME/.vim
 readonly VIM_FILE=$HOME/.vimrc
 
 # .vimディレクトリがない場合はエラー
-if [ ! -d $VIM_DIR ]; then
+if [ ! -d $VIM_DIR -a ! -d $VIM_DIR/.git ]; then
 	echo "not found $VIM_DIR"
 	echo "git clone http://github.com/rondeproject/vimrc.git $VIM_DIR"
 	exit
@@ -14,4 +14,9 @@ fi
 if [ -f $VIM_DIR/.vimrc -a ! -f $VIM_FILE ]; then
 	ln -s $VIM_DIR/.vimrc $VIM_FILE
 fi
+
+# git 設定
+git config --local user.name Ronde
+git config --local user.email none
+
 
