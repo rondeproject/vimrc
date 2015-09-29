34c4f5922f0f9296d1b69c9bfbc151545bf923ab
Ronde <none>
2015-09-29 17:11:40 +0900

    シンボリックリンクを作らないように変更

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 readonly VIM_DIR=$HOME/.vim
-readonly VIM_FILE=$HOME/.vimrc
 
 # .vimディレクトリがない場合はエラー
 if [ ! -d $VIM_DIR -a ! -d $VIM_DIR/.git ]; then
@@ -10,13 +9,10 @@ if [ ! -d $VIM_DIR -a ! -d $VIM_DIR/.git ]; then
 	exit
 fi
 
-# .vimrcをリンク
-if [ -f $VIM_DIR/.vimrc -a ! -f $VIM_FILE ]; then
-	ln -s $VIM_DIR/.vimrc $VIM_FILE
-fi
-
 # git 設定
+pushd $VIM_DIR >/dev/null
 git config --local user.name Ronde
 git config --local user.email none
+popd >/dev/null
 
 
